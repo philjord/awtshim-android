95671af046c5aa3be6420ac588c5e07bd09ef731
phil <philjord@ihug.co.nz>
2017-02-11 15:54:01 +1300

    some weak refs added for performance

--- awtshim-android/src/javaawt/image/VMRaster.java
+++ awtshim-android/src/javaawt/image/VMRaster.java
@@ -1,12 +1,13 @@
 package javaawt.image;
 
-import android.support.v4.graphics.BitmapCompat;
-
-import java.nio.ByteBuffer;
+import java.lang.ref.WeakReference;
 import java.nio.IntBuffer;
+
+import android.support.v4.graphics.BitmapCompat;
 
 public class VMRaster implements Raster
 {
+	protected WeakReference<VMDataBufferInt> dataBufferRef;
 	protected android.graphics.Bitmap delegate = null;
 
 	public VMRaster(android.graphics.Bitmap delegate)
@@ -14,53 +15,49 @@ public class VMRaster implements Raster
 		this.delegate = delegate;
 	}
 
+	@Override
 	public android.graphics.Bitmap getDelegate()
 	{
 		return delegate;
 	}
 
+	@Override
 	public void getDataElements(int w, int h, Object pixel)
 	{
 		throw new UnsupportedOperationException();//delegate.getDataElements(w, h, pixel);
 	}
 
+	@Override
 	public int getNumDataElements()
 	{
 		throw new UnsupportedOperationException();//	return delegate.getNumDataElements();
 	}
 
+	@Override
 	public int getTransferType()
 	{
 		throw new UnsupportedOperationException();//	return delegate.getTransferType();
 	}
 
-	public boolean goint = true;
+	@Override
 	public DataBuffer getDataBuffer()
 	{
-
-
 		//Ok, so ImageComponent2DRetained has a getImage call...
 		// which calls createBufferedImage
 		// which creates a new BufferedImage, then calls ImageComponentRetained.copyByLine to get data into it!!
 		// so it wants the returned Databuffer to actually be the backing int array of the image to write into it!
 		// but obviously here I create a new buffer and copy my data into it, and that new buffer
 		// backs nothing and is eventually dropped
-
-		if(goint)
+		if (dataBufferRef != null && dataBufferRef.get() != null)
 		{
-			IntBuffer buffer1 = IntBuffer.allocate(BitmapCompat.getAllocationByteCount(delegate)/4);
-			delegate.copyPixelsToBuffer(buffer1);
-			return new VMDataBufferInt(buffer1);
-		}
-		else
-		{
-			//ByteBuffer buffer1 = ByteBuffer.allocate(delegate.getHeight() * delegate.getRowBytes() / 4);
-			ByteBuffer buffer1 = ByteBuffer.allocate(BitmapCompat.getAllocationByteCount(delegate));
-			delegate.copyPixelsToBuffer(buffer1);
-			return new VMDataBufferByte(buffer1);
+			return dataBufferRef.get();
 		}
 
-
+		IntBuffer buffer1 = IntBuffer.allocate(BitmapCompat.getAllocationByteCount(delegate) / 4);
+		delegate.copyPixelsToBuffer(buffer1);
+		VMDataBufferInt ret = new VMDataBufferInt(buffer1);
+		dataBufferRef = new WeakReference<VMDataBufferInt>(ret);
+		return ret;
 
 	}
 }
--- awtshim-android/src/javaawt/image/VMWritableRaster.java
+++ awtshim-android/src/javaawt/image/VMWritableRaster.java
@@ -2,14 +2,16 @@ package javaawt.image;
 
 import android.support.v4.graphics.BitmapCompat;
 
-import java.nio.ByteBuffer;
+import java.lang.ref.WeakReference;
 import java.nio.IntBuffer;
 
 import javaawt.image.WritableRaster;
 
 public class VMWritableRaster extends VMRaster implements WritableRaster
-{
- 	public VMWritableRaster(android.graphics.Bitmap delegate)
+{	
+	private WeakReference<int[]> buff;
+
+	public VMWritableRaster(android.graphics.Bitmap delegate)
 	{
 		super(delegate);
 
@@ -21,13 +23,30 @@ public class VMWritableRaster extends VMRaster implements WritableRaster
 		return delegate;
 	}
 
+	@Override
 	public int[] getDataElements(int i, int j, int width, int height, Object object)
 	{
-		//TODO: possibly garbage?
-		IntBuffer buffer1 = IntBuffer.allocate(BitmapCompat.getAllocationByteCount(delegate)/4);
-		delegate.copyPixelsToBuffer(buffer1);
-		return buffer1.array();
-		//throw new UnsupportedOperationException();//return (int[]) delegate.getDataElements(i, j, width, height, object);
+
+		if (buff != null && buff.get() != null)
+		{
+			return buff.get();
+		}
+		
+		// check parents dataBufferRef
+		if(dataBufferRef !=null && dataBufferRef.get()!=null)
+		{
+			int[] data = dataBufferRef.get().getData();
+			buff = new WeakReference<int[]>(data);
+			return data;
+		}
+		else
+		{
+			IntBuffer buffer1 = IntBuffer.allocate(BitmapCompat.getAllocationByteCount(delegate) / 4);
+			delegate.copyPixelsToBuffer(buffer1);
+			int[] data = buffer1.array();
+			buff = new WeakReference<int[]>(data);
+			return data;
+		}
 	}
 
 }
