84a19fa0d7fb6e423312a51c50f6078ead79fd7b
phil <philjord@ihug.co.nz>
2017-03-07 09:38:11 +1300

    font has set size and gettextbounds

--- awtshim-android/src/javaawt/VMFont.java
+++ awtshim-android/src/javaawt/VMFont.java
@@ -1,6 +1,8 @@
 package javaawt;
 
+import android.graphics.Rect;
 import android.graphics.Typeface;
+import javaawt.geom.Rectangle2D;
 
 /**
  * Created by phil on 12/13/2016.
@@ -8,6 +10,8 @@ import android.graphics.Typeface;
 
 public class VMFont extends Font
 {
+	private android.graphics.Paint fontSizingPaint = new android.graphics.Paint();
+	
 	private Typeface typeface;
 	private int size = 12;
 
@@ -15,8 +19,10 @@ public class VMFont extends Font
 	{
 		this.typeface = typeface;
 		this.size = size;
+		fontSizingPaint.setTypeface(typeface);
 	}
 
+	@Override
 	public Object getDelegate()
 	{
 		return typeface;
@@ -26,5 +32,20 @@ public class VMFont extends Font
 	public int getSize()
 	{
 		return size;
+	}
+	@Override
+	public void setSize(int fontSize)
+	{
+		this.size = fontSize;		
+	}
+	
+	@Override
+	public Rectangle2D getStringBounds(String text)
+	{				
+		fontSizingPaint.setTextSize(getSize());
+		Rect r = new Rect();
+		fontSizingPaint.getTextBounds(text, 0, text.length(), r);
+		Rectangle2D textBounds = new Rectangle(r.left, r.bottom, r.right - r.left, r.top - r.bottom);
+		return textBounds;
 	}
 }
